63c19fb8ec33b6632ec09128a8dff0cc96103c41
ixlei <1099461262@qq.com>
2015-12-10 14:41:53 -0800

    customer nav header reducer

---
+++ src/main/resources/scripts/reducers/navHeader.js
@@ -0,0 +1,18 @@
+'use strict';
+import { ACTIVE_INDEX } from '../constants/customerActionTypes';
+
+const initState = {
+	index: 0
+};
+
+function setActiveIndex(state = initState, action) {
+	if(Object.is(ACTIVE_INDEX, action.type)) {
+		return Object.assign({}, state, {
+			index:action.index
+		});
+	}
+	return state;
+}
+
+export  { setActiveIndex as default };
+
