e0c9da74ae87118044d4a2d04d1ad495b5e5f716
ixlei <1099461262@qq.com>
2015-12-10 15:53:16 -0800

    create store configure

---
+++ src/main/resources/scripts/store/configStore.js
@@ -0,0 +1,16 @@
+'use strict';
+import {createStore, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+import rootReducer from '../reducers/index';
+
+const loggerMiddleware = createLogger();
+
+const createStoreWithMiddleware = applyMiddleware(
+	thunkMiddleware,
+	loggerMiddleware
+)(createStore);
+
+export default configStore(initState) {
+	return createStoreWithMiddleware(rootReducer, ininState);
+}
