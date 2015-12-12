867dc6cb3319ef3cdd067bb48930c39820c08f12
ixlei <1099461262@qq.com>
2015-12-11 19:38:06 -0800

    remove

--- src/main/resources/scripts/customerEntry.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import React from 'react';
-import {render} from 'react-dom';
-import customerIndex from './index/customerIndex';
-render(<customerIndex />, document.querySelector('#main'));
