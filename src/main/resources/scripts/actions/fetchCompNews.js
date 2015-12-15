a5a7b762499629131fb87a6b3857124bdb6a6049
ixlei <1099461262@qq.com>
2015-12-14 21:42:11 -0800

    news actions

---
+++ src/main/resources/scripts/actions/fetchCompNews.js
@@ -0,0 +1,48 @@
+'use strict';
+
+import fetch from 'isomorphic-fetch';
+import * as types from '../constants/customerActionType';
+
+export function fetchCompNews(news) {
+  return dispatch => {
+  	dispatch(requestCompNews(news));
+  	return fetch('/react-spring/customer/getCompNews')
+  	  .then(checkSatus)
+  	  .then(response => response.json())
+  	  .then(json => dispatch(receiveCompNews(json)))
+  	  .catch(error => dispatch(failureRequest(error)));
+  }
+    
+}
+
+function requestCompNews(newsType) {
+  return {
+	type: types.REQUEST_COMPNEWS,
+	newsType
+  }
+}
+
+function receiveCompNews(news) {
+  return {
+  	type: types.RECEIVE_COMPNEWS,
+  	news
+  }
+}
+
+function failureRequest(err) {
+	return {
+		type: types.FAILURE_COMPNEWS,
+		err
+	}
+}
+
+function checkSatus(response) {
+	if(response.status >= 200 && response.status < 300 || 
+		response.status === 304) {
+		return response;
+	}
+
+	let error =  new Error(response.statusText);
+	error.response = response;
+	throw error;
+}
