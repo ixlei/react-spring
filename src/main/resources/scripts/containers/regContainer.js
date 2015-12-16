35ef067b0c230ecaea21ebb57a047f0e2eb9d171
ixlei <1099461262@qq.com>
2015-12-15 22:33:20 -0800

    container regContainer

---
+++ src/main/resources/scripts/containers/regContainer.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+import RegStep from '../components/regStep';
+
+class RegContainer extends Component {
+  render() {
+	return (
+	  <RegStep {...this.props} />
+	)
+  }
+}
+
+function mapStateToProps(state) {
+  let {reg: {regStep}} = state;
+  return {
+  	regStep
+  }
+}
+
+export default connect(mapStateToProps)(RegContainer);
