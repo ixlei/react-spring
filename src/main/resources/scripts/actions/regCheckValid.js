075ec49383904424a3d6ed38ad42bde9c6cc2c38
ixlei <1099461262@qq.com>
2015-12-21 10:04:35 -0800

    reg fetch check

---
+++ src/main/resources/scripts/actions/regCheckValid.js
@@ -0,0 +1,51 @@
+'use strict';
+
+import fetch from 'isomorphic-fetch';
+import * as types from '../constants/customerActionType';
+
+export function postCheckValid(checkKind,body) {
+  return (dispatch, getState) => {
+  	dispatch(post(checkKind));
+  	let {reg:{userType}} = getState();
+    body.append('userType', userType);
+    return fetch('/customer/' + checkKind, {
+      method: 'post',
+      body
+    })
+    .then(checkSatus)
+    .then(response => response.json())
+    .then(json => dispatch(receiveCheckInvalid(json)))
+    .catch(json => dispatch(failureCheckInvalid(json)));
+  }
+}
+
+function post(postCheck) {
+   return {
+     type: types.POST_CHECKVALID,
+   	 postCheck
+   }
+}
+
+function checkSatus(response) {
+	if(response.status >= 200 && response.status < 300 || 
+		response.status === 304) {
+		return response;
+	}
+	let error =  new Error(response.statusText);
+	error.response = response;
+	throw error;
+}
+
+function receiveCheckInvalid(entities) {
+  return {
+  	type: types.RECEIVE_CHECKVALID,
+  	entities
+  }
+}
+
+function failureCheckInvalid(error) {
+  return {
+  	type: types.FAILURE_CHECKVALID,
+  	error
+  }
+}
