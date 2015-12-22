1461fca4cb5aa1ccc38418c8c033d32b245b2133
ixlei <1099461262@qq.com>
2015-12-22 10:36:17 -0800

    submit container

---
+++ src/main/resources/scripts/containers/FormSubmitContainer.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {Navigation} from 'react-router';
+import FormSubmit from '../components/FormSubmit';
+
+class FormSubmitContainer extends Component {
+ 
+  render() {
+  	return (<FormSubmit {...this.props} />)
+  }
+}
+
+function mapStateToProps(state) {
+  const {reg: {itemState, iagree, userType}} = state;
+  let ready = false;
+  if(userType === 'investor') {
+  	ready = itemState.every(function(data) {
+  	  let {id, valid} = data;
+  	  if(id === 'code' || id === 'companyname') {
+  	  	return true;
+  	  }
+  	  return valid === 'valid';
+  	})
+  } else {
+    ready = itemState.every(function(data) {
+  	  let {id, valid} = data;
+  	  if(id === 'name' || id === 'IdCard') {
+  	  	return true;
+  	  }
+  	  return valid === 'valid';
+  	})
+  }
+  return {
+  	ready: !(iagree && ready)
+  };
+}
+
+export default connect(mapStateToProps)(FormSubmitContainer);
