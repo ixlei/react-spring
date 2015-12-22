45bb17fbb75d4f8c5c85d7ebba234d1acdec0794
ixlei <1099461262@qq.com>
2015-12-22 10:34:17 -0800

    form checkbox commponent

---
+++ src/main/resources/scripts/components/FormCheckbox.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import React, {Component, PropTypes} from 'react';
+import {Link} from 'react-router';
+import {iagree} from '../actions/reg';
+
+export default class FormCheckbox extends Component {
+
+  change(e) {
+   const {dispatch} = this.props;
+   dispatch(iagree());
+  }
+
+  render() {
+  	return (<div>
+  	  <span className="label"></span>
+      <input type="checkbox" name="iagree"
+      value="agree"
+      checked={this.props.iagree}
+      className="checke" onChange={this.change.bind(this)}/>
+      <span>我同意<Link to={"/protopl"} >《网站服务协议》</Link></span>
+    </div>
+  	) 
+  }
+}
+
