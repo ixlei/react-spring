ae4a857bc77fd23a84610676e3dc72ed9029b48d
ixlei <1099461262@qq.com>
2015-12-22 10:37:37 -0800

    idcard container

---
+++ src/main/resources/scripts/containers/FormIdcardContainer.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import FormIdcard from '../components/FormIdcard';
+
+class FormIdcardContainer extends Component {
+  render() {
+  	return (
+  	  <FormIdcard {...this.props} />
+  	)
+  }
+}
+
+function mapStateToProps(state) {
+  const {reg:{IsFocus, itemState, userType}} = state;
+  const {valid, tips} = itemState[4];
+  return {
+  	IsFocus,
+  	valid,
+  	tips,
+  	userType
+  }
+}
+
+export default connect(mapStateToProps)(FormIdcardContainer);
