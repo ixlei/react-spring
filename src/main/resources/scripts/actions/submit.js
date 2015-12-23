805f7cb0751da92acd50d18e6ce232d1a7138610
ixlei <1099461262@qq.com>
2015-12-23 08:37:58 -0800

    action submit

---
+++ src/main/resources/scripts/actions/submit.js
@@ -0,0 +1,50 @@
+'use strict';
+import fetch from 'isomorphic-fetch';
+import * as types from '../constants/customerActionType';
+
+export function subAction(arg) {
+   const {action, body, subType} = arg;
+   return (dispatch) => {
+   	 dispatch(submitType(subType));
+   	 return fetch(action, {
+   	 	method: 'post',
+   	 	body
+   	 }).then(checkSatus)
+   	   .then(response => response.json())
+       .then(json => {dispatch(submitSuccess(json))})
+       .catch(error => {dispatch(failureSubmit(error))
+     })
+
+   }
+}
+
+function submitType(subType) {
+  return {
+  	type: types.SUBTYPE,
+  	subType
+  }
+}
+
+function checkSatus(response) {
+	if(response.status >= 200 && response.status < 300 || 
+		response.status === 304) {
+		return response;
+	}
+	let error =  new Error(response.statusText);
+	error.response = response;
+	throw error;
+}
+
+export function submitSuccess(res) {
+  return {
+  	type: types.SUBMITSUCCESS,
+  	res
+  }
+}
+
+export function failureSubmit(error) {
+  return {
+  	type: types.FAILURESUBMIT,
+  	error
+  }
+}
