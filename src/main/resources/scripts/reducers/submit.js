19da099abc7591fb25511ae11865e39a74980e34
ixlei <1099461262@qq.com>
2015-12-23 08:40:12 -0800

    subit reducer

---
+++ src/main/resources/scripts/reducers/submit.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import * as types from '../constants/customerActionType';
+
+const initState = {
+  subType: '',
+  subSuccess: false,
+  isFetching: false
+}
+
+export function subReducer(state = initState, action) {
+  switch(action.type) {
+  	case types.SUBTYPE:
+  	  return Object.assign({}, state, {
+  	  	subType: action.subType,
+  	  	isFetching: true
+  	  })
+  	case types.SUBMITSUCCESS:
+  	  return Object.assign({}, state, {
+  	  	subSuccess: (action.res.post === 'success'),
+  	  	isFetching: false
+  	  })
+  	case types.FAILURESUBMIT:
+  	  return Object.assign({}, state, {
+  	  	subSuccess: false,
+  	  	isFetching: false
+  	  })
+  	default:
+  	  return state;
+  }
+}
