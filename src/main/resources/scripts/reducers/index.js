1621eefe5d78dd3472bc69b43369e87ebd1d399d
ixlei <1099461262@qq.com>
2015-12-23 08:42:27 -0800

    combineReducers submit

--- src/main/resources/scripts/reducers/index.js
+++ src/main/resources/scripts/reducers/index.js
@@ -5,12 +5,14 @@ import activeIndex from './navHeader';
 import {compNews} from './companyNews';
 import {getNews} from './comNews';
 import {reg} from './reg';
+import {subReducer} from './submit';
 
 const rootReducer = combineReducers({
 	activeIndex,
 	compNews,
 	getNews,
-	reg
+	reg,
+	subReducer
 });
 
 export default rootReducer;
