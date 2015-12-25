6ac41a331de7acfe02f3f125cd9211fc82949177
ixlei <1099461262@qq.com>
2015-12-25 10:36:32 -0800

    change route

--- src/main/resources/scripts/routes/customerRoute.js
+++ src/main/resources/scripts/routes/customerRoute.js
@@ -2,12 +2,13 @@
 
 import React, {Component} from 'react';
 import {Router, Route, Link, IndexRoute} from 'react-router';
-import { createHistory, useBasename } from 'history';
+import {createHistory, useBasename} from 'history';
 import CustomerApp from '../containers/customerApp';
 import CustomerIndex from '../containers/customerIndex';
 import InvestmentHall from '../components/investmentHall';
 import Reg from '../components/reg';
 import RegForm from '../components/regForm';
+import InvestorNextstep from '../components/investorNextStep';
 
 const history = useBasename(createHistory)({
   basename: '/customer'
@@ -20,9 +21,9 @@ export default class CustomerRoute extends Component {
 		  <Route path="/" component={CustomerApp}>
 			<IndexRoute component={CustomerIndex} />
 			<Route path="index/:index" component={CustomerIndex} />
-            <Route path="invest/:index" 
-              component={InvestmentHall} />
+            <Route path="invest/:index" component={InvestmentHall} />
             <Route path="reg" component={Reg}>
+              <Route path="investorNext" component={InvestorNextstep} />
               <IndexRoute component={RegForm} />
             </Route>
 		  </Route>
