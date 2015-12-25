ec379235bb4ea437b22eb508134e60b4f06632b7
ixlei <1099461262@qq.com>
2015-12-25 10:35:33 -0800

    investorreg next component

---
+++ src/main/resources/scripts/components/investorNextStep.js
@@ -0,0 +1,68 @@
+'use strict';
+
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+import FormInput from './FormInput';
+import {regStep} from '../actions/reg';
+import FormSubmit  from './FormSubmit';
+import {subAction} from '../actions/submit';
+
+class InvestorNextstep extends Component {
+  componentDidMount() {
+    const {dispatch} = this.props;
+    dispatch(regStep(1))
+  }
+
+  submit(e) {
+    e.preventDefault();
+    const {dispatch} = this.props;
+    let form = document.forms.reg;
+    let formdata = new FormData(form);
+    let arg = {
+      action: '/customer/investorNextStep',
+      body: formdata,
+      subType: 'reg2'
+    }
+    dispatch(subAction(arg));
+  }
+  
+  componentWillReceiveProps(nextProps) {
+     const {subType, subSuccess, isFetching} = nextProps;
+    if(subType ==='reg2' && subSuccess && !isFetching) {
+      this.context.history.pushState(null, '/reg/investorNext');
+    }
+  }
+
+  render() {
+  	return (
+  	<form name="reg" onSubmit={this.submit.bind(this)}>
+  	  <div id="left-reg">
+	    <ul>
+	      <li><FormInput text="公司名称:" name="companyName"/></li>
+	      <li><FormInput text="法定代表人:" name="legalRepresentative"/></li>
+	      <li><FormInput text="法定代表委托人:" name="legalRepresentativewt" /></li>
+	      <li><FormInput text="营业执照注册号:" name="businesslicence" /></li>
+	      <li><FormInput text="注册资本:" name="registerCapital" /></li>
+	      <li><FormInput text="注册地址:" name="registerAddress" /></li>
+	      <li><FormInput text="所在地区:" name="companyAddress" /></li>
+	      <li><FormInput text="投资地区:" name="investAddress"/></li>
+	      <li><FormInput text="投资周期:" name="investCycle" /></li>
+	      <li><FormInput text="总部地址:" name="headquartersAddress"/></li>
+	      <li><FormInput text="咨询电话:" name="phonenumber"/></li>
+	      <li><FormSubmit ready={false} /></li>
+	    </ul>
+	  </div>
+  	</form>
+  	)
+  }
+}
+
+function mapStateToProps(state) {
+  return {};
+}
+
+InvestorNextstep.contextTypes = {
+  history: PropTypes.object.isRequired
+}
+
+export default connect(mapStateToProps)(InvestorNextstep);
