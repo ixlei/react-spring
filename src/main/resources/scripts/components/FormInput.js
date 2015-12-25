b8d5b9a08b8630143cc99dbd23dfc0fe7aa7bcd0
ixlei <1099461262@qq.com>
2015-12-25 09:44:36 -0800

    forminput

---
+++ src/main/resources/scripts/components/FormInput.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React, {Component, PropTypes} from 'react';
+
+export default class FormInput extends Component {
+  render() {
+  	const {text, name} = this.props;
+    return (<div>
+      <span className="label">{text}</span> 
+	  <input type="text" name={name} className="reg-input"/>
+	</div>
+    )
+  }
+}
+
+FormInput.propTypes = {
+  text: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
+}
+
