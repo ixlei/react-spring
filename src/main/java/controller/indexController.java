85221397436eee4915334f1aa03d9d692e6c74d9
ixlei <1099461262@qq.com>
2015-12-28 20:53:43 -0800

    response add cors

--- src/main/java/controller/indexController.java
+++ src/main/java/controller/indexController.java
@@ -4,12 +4,15 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
+
 
 @Controller
 public class indexController {
 	
 	@RequestMapping("/")
-	public String showIndex() {
+	public String showIndex(HttpServletResponse res) {
+		res.setHeader("Access-Control-Allow-Origin", "*");
 		return "customer/index";
 	}
 
