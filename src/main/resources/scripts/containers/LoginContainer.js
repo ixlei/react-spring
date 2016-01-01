aa8b983963ce3b9df38d01fd3f27c03a46cee971
ixlei <1099461262@qq.com>
2015-12-31 21:47:56 -0800

    loginContainer component

---
+++ src/main/resources/scripts/containers/LoginContainer.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import React, {Component, PropTypes} from 'react';
+import LoginForm from '../components/LoginForm';
+import {connect} from 'react-redux';
+import Unslider from '../components/unslider';
+
+require('../../styles/login.scss');
+
+class LoginFormContainer extends Component {
+  render() {
+  	let unslider = {
+  	  list: ['banner1.jpg', 'banner2.png', 'banner3.png'],
+	  arrowImgUrl: ['btn-pre.png', 'btn-next.png']
+  	}
+
+  	return (<div id="content" className="loginPage">
+  	  <Unslider {...unslider} />
+  	  <div id="login">
+  	    <LoginForm {...this.props} />
+  	  </div>
+  	</div>
+  	)
+  }
+}
+
+function mapStateToProps(state) {
+  const {subReducer: {subType, subSuccess, isFetching, text}} = state;
+  return {
+  	subType,
+  	subSuccess,
+  	isFetching,
+  	text
+  }
+}
+
+export default connect(mapStateToProps)(LoginFormContainer);
