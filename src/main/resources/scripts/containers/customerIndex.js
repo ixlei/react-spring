a5931c1fdfd44ce66b0813ea4d3828f2c0ae345b
ixlei <1099461262@qq.com>
2016-01-02 09:09:22 -0800

    format customerIndex

--- src/main/resources/scripts/containers/customerIndex.js
+++ src/main/resources/scripts/containers/customerIndex.js
@@ -19,21 +19,21 @@ class CustomerIndex extends Component {
 	}
 
 	render() {
-		return (
-			<div id="content">
-				<UnsliderContainer />
-				<CompNewsContainer />
-				<div id="invest-center">
-				  <p id="inv-title">投资中心</p>
-				  <InvestorTitle />
-				</div>
-				<div id="news">
-				  <p id="zixun">资讯</p>
-				  <NewsConatiner />
-				</div>
-			</div>
-		)
-	}
+	  return (
+	    <div id="content">
+	      <UnsliderContainer />
+		  <CompNewsContainer />
+		  <div id="invest-center">
+			<p id="inv-title">投资中心</p>
+			<InvestorTitle />
+		  </div>
+		  <div id="news">
+			<p id="zixun">资讯</p>
+			<NewsConatiner />
+		  </div>
+		</div>
+	  )
+   }
 }
 
 function mapStateToProps(state) {
