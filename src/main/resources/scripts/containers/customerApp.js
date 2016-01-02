0e01177ef298c65136b0c6babc002ca7e3ac26fa
ixlei <1099461262@qq.com>
2016-01-02 09:08:54 -0800

    format cstomerApp

--- src/main/resources/scripts/containers/customerApp.js
+++ src/main/resources/scripts/containers/customerApp.js
@@ -4,14 +4,14 @@ import React, {Component} from 'react';
 import NavContainer from '../containers/customerNavContainers';
 
 export default class CustomerApp extends Component {
-	render() {
-		return (
-		  <div>
-			<NavContainer />
-			  <div id="main">
-				{this.props.children}
-			  </div>
-		  </div>
-		);
-	}
+  render() {
+	return (
+	  <div>
+		<NavContainer />
+		<div id="main">
+		  {this.props.children}
+	    </div>
+	  </div>
+	);
+  }
 }
