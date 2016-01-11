1c1e38ed8b7515ec4685ef04cf889464cd12f44e
ixlei <1099461262@qq.com>
2016-01-11 14:37:34 -0800

    delete file

--- src/main/resources/scripts/entry/customerEntry.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-import React, {Component} from 'react';
-import {render} from 'react-dom';
-import CustomerIndex from '../index/customerIndex';
-
-require('../../styles/customerHeader.scss');
-
-render(<CustomerIndex />, document.querySelector('#mainPage'));
