dfce2870d254c62a9be17afef9f2b08bb4df2c67
ixlei <1099461262@qq.com>
2016-01-22 12:55:13 -0800

    format js

--- src/main/resources/scripts/components/ListItem.js
+++ src/main/resources/scripts/components/ListItem.js
@@ -4,21 +4,20 @@ import React, {Component, PropTypes} from 'react';
 import {Link} from 'react-router';
 
 export default class ListItem extends Component {
-	render() {
-		return (
-			<ul>
-			    {this.props.list.map((list, index) => (
-				<li key={list.addr}>
-					<Link to={list.addr}><span>{list.text}</span>
-					</Link>
-				</li>
-				))
-			}
-			</ul>
-		);
-	}
+  render() {
+    return (<ul>
+	  {this.props.list.map((list, index) => (
+	  <li key={list.addr}>
+		<Link to={list.addr}><span>{list.text}</span>
+		</Link>
+	  </li>
+	  ))
+	 }
+	</ul>
+   );
+  }
 }
 
 ListItem.propTypes = {
-	list: PropTypes.array.isRequired
+  list: PropTypes.array.isRequired
 };
