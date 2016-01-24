231eee8ed743e0a44330fed3cada5cd794359577
ixlei <1099461262@qq.com>
2016-01-24 12:18:07 -0800

    add the search construct url function

--- src/main/resources/scripts/utils/constructUrl.js
+++ src/main/resources/scripts/utils/constructUrl.js
@@ -11,3 +11,13 @@ export function constructFetchComNews() {
 export function constructNewsUrl(newsType) {
   return `/news/${newsType}`;
 }
+
+export function constructSocketUrl(args) {
+  const {userType, socketType, socketTime} = args;
+  return `/${userType}/socketManage/${socketType}/?socketTime=${socketTime}`;
+}
+
+export function constructSocketSearchUrl(args) {
+  const {userType, socketType, socketTime, query} = args;
+  return `/${userType}/manage/${socketType}?socketTime=${socketTime}&query=${query}`;
+}
