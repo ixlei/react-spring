61c346a18ed8a208bdae0404ee95561f2cbcecc2
ixlei <1099461262@qq.com>
2016-01-24 14:37:42 -0800

    user update

--- src/main/resources/scripts/actions/user.js
+++ src/main/resources/scripts/actions/user.js
@@ -4,6 +4,7 @@ import * as types from '../constants/customerActionType';
 import fetch from 'isomorphic-fetch';
 import {checkStatus} from '../utils/fetchStatus';
 import {constructUserUrl, constructChatUrl} from '../utils/userUrl';
+import {constructIfollerUrl} from '../utils/constructUrl';
 
 export function fetchUser() {
   return (dispatch, getState) => {
@@ -32,49 +33,50 @@ export function fetchFriends() {
     .catch(err => dispatch(failureFriends(err)))
   }
 }
+
 export function userType(userType) {
   return {
   	type: types.USERTYPE,
   	userType
-  }
+  };
 }
 
 function receiveUserInfo(entity) {
   return {
   	type: types.USERINFO,
   	entity
-  }
+  };
 }
 
 function failureUser(error) {
   return {
     type: types.USERINFOFAILURE,
     error
-  }
+  };
 }
 
 function requestUser() {
   return {
   	type: types.REQUESTUSER
-  }
+  };
 }
 
 function requestFriends() {
   return {
     type: types.REQUESTFRIENDS
-  }
+  };
 }
 
 function receiveFriends(entity) {
   return {
     type: types.RECEIVEFRIENDS,
     entity
-  }
+  };
 }
 
 function failureFriends(error) {
   return {
     type: types.FAILUREFRIENDS,
     error
-  }
+  };
 }
