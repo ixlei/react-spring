3bb5dd63b9c58752c89857c4eb69e856605b7894
ixlei <1099461262@qq.com>
2016-01-25 14:49:01 -0800

    change the logo function to logo component

--- src/main/resources/scripts/components/customerNavHeader.js
+++ src/main/resources/scripts/components/customerNavHeader.js
@@ -3,70 +3,59 @@
 import React, {Component, PropTypes} from 'react';
 import {Link} from 'react-router';
 import ListItem from './ListItem';
+import Logo from './logo';
 
 export default class Nav extends Component {
- 	constructor(props) {
- 		super(props);
- 	}
-
- 	renderLogo() {
- 		return (
-			<div id="nav-logo">
-		        <span>
-		            <img src={this.props.logoUrl} id="logo" /> 
-		        </span>
-	        </div>
- 		);
- 	}
 
  	renderGoList() {
- 		let activeIndex = 0;
- 		return (
- 			<div id="nav-list">
-		      <ul>
-		      	{this.props.goList.map((list, index) =>(
-	      		<li key={list.addr}>
-	      			<Link to={'customer/' + list.addr + '/' + index}>
-	      				<span className=
-	      				{parseInt(this.props.activeIndex.index) === index 
-	      					? 'nav-item active' : 'nav-item'}>
-	      				{list.text}</span>
-	      			</Link>
-	      			{(() =>{
-	      				if(Object.is(3, index)) {
-	  					return (<div id="service-subnav">
-							<ListItem list = {this.props.subNav} />
-						</div>)
-	      				}
-	      			})()
-	      		}
-	      		</li>
-		      	))
-		    }
-	        </ul>
-		</div>
- 		);
+	    let activeIndex = 0;
+		return (
+			<div id="nav-list">
+	      <ul>
+	      	{this.props.goList.map((list, index) =>(
+      		<li key={list.addr}>
+      			<Link to={'customer/' + list.addr + '/' + index}>
+      				<span className=
+      				{parseInt(this.props.activeIndex.index) === index 
+      					? 'nav-item active' 
+      					: 'nav-item'}>
+      				{list.text}</span>
+      			</Link>
+      			{(() =>{
+      				if(Object.is(3, index)) {
+  					return (<div id="service-subnav">
+					  <ListItem list = {this.props.subNav} />
+					</div>)
+      				}
+      			})()
+      		}
+      		</li>
+	      	))
+	    }
+        </ul>
+	</div>
+	  );
  	}
 
  	renderLogin() {
- 		return (
- 			<div id="nav-login">
- 				<ListItem list= {this.props.loginList} />
- 			</div>
- 		);
+ 	  return (
+		<div id="nav-login">
+		  <ListItem list= {this.props.loginList} />
+		</div>
+ 	 );
  	}
 
  	render() {
- 		return (
- 			<div id="header">
- 				<div id="header-nav">
- 					{this.renderLogo()}
- 					{this.renderGoList()}
- 					{this.renderLogin()}
- 				</div>
- 			</div>
- 		);
- 	}
+ 	  return (
+		<div id="header">
+		  <div id="header-nav">
+		  <Logo />
+		  {this.renderGoList()}
+		  {this.renderLogin()}
+		  </div>
+		</div>
+ 	 );
+  }
  }
 
 Nav.PropTypes = {
