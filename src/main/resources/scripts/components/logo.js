3afcec92c6686de46c8c7eb040ff7b77abc92a4d
ixlei <1099461262@qq.com>
2016-01-25 14:49:48 -0800

    logo component

---
+++ src/main/resources/scripts/components/logo.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React, {Component, PropTypes} from 'react';
+
+export default class Logo extends Component {
+  render() {
+    return (
+      <div id="nav-logo">
+        <span>
+          <img src="/images/logo-last.png" id="logo" /> 
+        </span>
+      </div>
+    )
+  }
+}
