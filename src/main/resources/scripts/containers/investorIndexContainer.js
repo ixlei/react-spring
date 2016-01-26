f581d87ccbc372dc7a06cce17e68ac1a3a02d2dc
ixlei <1099461262@qq.com>
2016-01-25 20:09:47 -0800

    format the file

--- src/main/resources/scripts/containers/investorIndexContainer.js
+++ src/main/resources/scripts/containers/investorIndexContainer.js
@@ -5,21 +5,24 @@ import InvestorIndex from '../components/investorIndex';
 import {connect} from 'react-redux';
 
 class InvestorIndexContainer extends Component {
-  
   render() {
-  	let unsliderTop = ['banner1.jpg', 'banner2.png', 'banner3.png'],
-		arrowImgUrl = ['btn-pre.png', 'btn-next.png'],
-		unsliderLeft = ['touzizhe1.png', 'touzizhe2.png'],
-		unsliderRight = ['touzizhe3.png', 'touzizhe4.png'];
-	let props = Object.assign({}, this.props, {unsliderTop, arrowImgUrl, unsliderLeft, unsliderRight});
   	return (
-  	  <InvestorIndex {...props} />
+  	  <InvestorIndex {...this.props} />
   	)
   }
 }
 
 function mapStateToProps(state) {
-  return {};
+  let unsliderTop = ['banner1.jpg', 'banner2.png', 'banner3.png'],
+    arrowImgUrl = ['btn-pre.png', 'btn-next.png'],
+    unsliderLeft = ['touzizhe1.png', 'touzizhe2.png'],
+    unsliderRight = ['touzizhe3.png', 'touzizhe4.png'];
+  return {
+    unsliderTop,
+    arrowImgUrl, 
+    unsliderLeft, 
+    unsliderRight
+  };
 }
 
 export default connect(mapStateToProps)(InvestorIndexContainer);
