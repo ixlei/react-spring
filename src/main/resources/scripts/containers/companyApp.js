bbcf07027fa446aaf23a5589d8c3dffbf260a374
ixlei <1099461262@qq.com>
2016-01-26 11:08:58 -0800

    format

--- src/main/resources/scripts/containers/companyApp.js
+++ src/main/resources/scripts/containers/companyApp.js
@@ -2,7 +2,7 @@
 
 import React, {Component,PropTypes} from 'react';
 import {connect} from 'react-redux';
-import CompanyNavContainer from '../containers/comapnyNavContainer';
+import CompanyNavContainer from '../containers/companyNavContainer';
 
 class CompanyApp extends Component {
   render() {
