13386adae3a651e6223b4166fe26875cc2bca9fb
ixlei <1099461262@qq.com>
2016-01-26 11:08:01 -0800

    format

---
+++ src/main/resources/scripts/components/companyIndex.js
@@ -0,0 +1,69 @@
+'use strict';
+
+import React, {Component, PropTypes} from 'react';
+import {Link} from 'react-router';
+import Unslider from './unslider';
+import {activeIndex} from '../actions/navHeader';
+import NewsConatiner from '../containers/listNewsContainer';
+
+export default class CompanyIndex extends Component {
+  
+  componentDidMount() {
+    const {dispatch, params: {index}} = this.props;
+    dispatch(activeIndex(parseInt(index)));
+  }
+
+  renderPushList() {
+  	const {arrowImgUrl, unsliderLeft, unsliderRight} = this.props;
+  	return (
+  	  <div id="invest-center">
+  	    <div id="inv-title">
+          <p id="inv-title">推荐行业</p>
+        </div>
+        <div id="invest-list">
+          <div id="left-banner">
+            <p>行业一</p>
+            <Unslider list={unsliderLeft} arrowImgUrl={arrowImgUrl}/>
+            <div className="invest-more">
+              <span><Link to={'/investor/investModel'}>查看更多行业></Link></span>
+            </div>
+          </div>
+          <div id="right-banner">
+            <p>行业二</p>
+            <Unslider list={unsliderRight} arrowImgUrl={arrowImgUrl}/>
+            <div className="invest-more">
+              <span><Link to={'/investor/investModel'}>查看更多行业></Link></span>
+            </div>
+          </div>
+        </div>
+      </div>
+  	)
+  }
+
+  rendernews() {
+    return (
+      <div id="news">
+        <p id="zixun">资讯</p>
+	     <NewsConatiner />
+      </div>
+    )
+  }
+
+ render() {
+  	const {arrowImgUrl,unsliderTop} = this.props;
+  	return (
+  	  <div id="content">
+  	    <Unslider arrowImgUrl={arrowImgUrl} list={unsliderTop}/>
+  	    {this.renderPushList()}
+  	    {this.rendernews()}
+  	  </div>
+  	)
+  }
+}
+
+CompanyIndex.propTypes = {
+  arrowImgUrl: PropTypes.arrayOf(PropTypes.string).isRequired,
+  unsliderTop: PropTypes.arrayOf(PropTypes.string).isRequired,
+  unsliderRight: PropTypes.arrayOf(PropTypes.string).isRequired,
+  unsliderLeft: PropTypes.arrayOf(PropTypes.string).isRequired
+}
