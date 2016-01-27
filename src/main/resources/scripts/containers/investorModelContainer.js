3384f84ea8ad97963c6dd359636557b8c7f196cf
ixlei <1099461262@qq.com>
2016-01-26 16:37:39 -0800

    delete the props of the area and industry

--- src/main/resources/scripts/containers/investorModelContainer.js
+++ src/main/resources/scripts/containers/investorModelContainer.js
@@ -6,13 +6,7 @@ import InvestorModel from '../components/investorModel';
 
 class InvestorModelContainer extends Component {
   render() {
-    let area = ['北京', '辽宁', '黑龙江', '吉林', '内蒙古', '云南','四川', '贵州', '湖南', '湖北', '河北'];
-    let industry = ['金融投资', '房地产', '能源', '化学化工', '矿产冶金', '汽车汽配', '批发零售', '收藏品','交通运输', '旅游酒店', '农林牧渔', '批发零售'];
-    const props = Object.assign({}, this.props, {
-      area,
-      industry
-    })
-  	return (<InvestorModel {...props} />)
+  	return (<InvestorModel {...this.props} />)
   }
 }
 
