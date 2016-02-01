be361b1d8c63fd0cf3f6d7f8f206a256b40a7f75
ixlei <1099461262@qq.com>
2016-01-31 21:42:58 -0800

    financeCompany component

---
+++ src/main/resources/scripts/components/financeCompany.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import React, {Component, PropTypes} from 'react';
+import {Link} from 'react-router';
+
+class FinanceCompany extends Component {
+  
+}
+
+
+export default FinanceCompany;
