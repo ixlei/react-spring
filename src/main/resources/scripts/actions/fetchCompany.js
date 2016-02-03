8668a464a3ea8c6dbc9bf15f68c40158aeb16e72
ixlei <1099461262@qq.com>
2016-02-03 15:48:15 -0800

    add the fetchFinance action

--- src/main/resources/scripts/actions/fetchCompany.js
+++ src/main/resources/scripts/actions/fetchCompany.js
@@ -4,37 +4,54 @@ import fetch from 'isomorphic-fetch';
 import {normalize, Schema, arrayOf} from 'normalizr';
 import * as types from '../constants/customerActionType';
 import {checkStatus} from '../utils/fetchStatus';
-import {constructCompNews} from '../utils/constructUrl';
+import {constructCompNews, constructFinanceUrl} from '../utils/constructUrl';
 
 export function fetchCompanyNews() {
-	return dispatch => {
-	  dispatch(requestCompany('compNews'));
-	  return fetch(constructCompNews())
-	    .then(checkStatus)
-	    .then(response => response.json())
-	    .then(json => dispatch(receiveCompanyNews(json)))
-	    .catch(json => dispatch(failureRequestCompanyNews(json)))
-	}
+  return dispatch => {
+	dispatch(requestCompany('compNews'));
+	return fetch(constructCompNews())
+	  .then(checkStatus)
+	  .then(response => response.json())
+	  .then(json => dispatch(receiveCompanyNews(json)))
+	  .catch(json => dispatch(failureRequestCompanyNews(json)))
+  }
 }
 
-function requestCompany(company) {
+export function fetchFinance() {
+  return dispatch => {
+  	dispatch(requestCompany('finance'));
+  	return fetch(constructFinanceUrl())
+  	  .then(checkStatus)
+  	  .then(response => response.json())
+  	  .then(json => dispatch(fetchFinance(json)))
+  	  .then(err => dispatch(failureRequestCompanyNews(err)))
+  }
+}
+
+function requestCompany(fetchType) {
   return {
 	type: types.REQUEST_COMPANY,
-	company
-  }
+	fetchType
+  };
 }
 
 function receiveCompanyNews(entities) {
   return {
 	type: types.RECEIVE_COMPANY,
 	entities
-  }
-
+  };
 }
 
 function failureRequestCompanyNews(entities) {
   return {
 	type: types.FAILURE_COMPANY,
 	entities
-  }
+  };
+}
+
+function fetchFinance(entity) {
+  return {
+  	type: types.FETCHFINANCE,
+  	entity
+  };
 }
