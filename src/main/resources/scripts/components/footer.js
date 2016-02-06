1bec3e2b1e5b158953b6d616f9d5c8771ee5bd3d
ixlei <1099461262@qq.com>
2016-02-06 14:21:00 -0800

    Footer component

---
+++ src/main/resources/scripts/components/footer.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React, {Component, PropTypes} from 'react';
+
+class Footer extends Component {
+  render() {
+  	return (<div id="footer">
+      <div id="foot-list">
+      </div>
+     </div>
+    )
+  }
+}
+
+export default Footer;
