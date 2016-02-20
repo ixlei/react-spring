23967d2f332e6c70c945f662073f7d1a4b49703f
ixlei <1099461262@qq.com>
2016-02-19 22:06:04 -0800

    add the variable check

--- src/main/resources/scripts/utils/varType.js
+++ src/main/resources/scripts/utils/varType.js
@@ -1,7 +1,35 @@
 'use strict';
 
-export function isArray(array) {
+export function isArray(value) {
   return Array.isArray 
-    ? Array.isArray(array) 
-    : Object.prototype.toString.call(array) === '[object Array]';
+    ? Array.isArray(value) 
+    : Object.prototype.toString.call(value) === '[object Array]';
+}
+
+export function isString(value) {
+  return typeof value === 'string';
+}
+
+export function isNumber(value) {
+  return typeof value === 'number';
+}
+
+export function isFunction(value) {
+  return typeof value === 'function';
+}
+
+export function isBoolean(value) {
+  return typeof value === 'boolean';
+}
+
+export function isObject(value) {
+  return value !== null && typeof value === 'object';
+}
+
+export function isDate(value) {
+  return Object.prototype.toString.call(value) === '[object Date]';
+}
+
+export function isRegExp(value) {
+  return Object.prototype.toString.call(value) === '[object RegExp]';
 }
