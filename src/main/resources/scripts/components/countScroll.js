87ad89e068febd03450e2fa850a9c3bfa9895a28
ixlei <1099461262@qq.com>
2016-03-09 15:57:45 -0800

    add the countScroll

--- src/main/resources/scripts/components/countScroll.js
+++ src/main/resources/scripts/components/countScroll.js
@@ -1,11 +1,33 @@
 'use strict';
 
 import React, {Component, PropTypes} from 'react';
+import {Motion, spring} from 'react-motion';
+import * as util from '../utils/varType';
 
 export default class CountScroll extends Component {
+
+  formatCount(count) {
+  	let regExp = /(?=(?!^)(\d{3})+$)/g;
+    if(util.isNumber(count)) {
+      return count.toString().replace(/^\d+/, (match) => {
+    	return match.replace(regExp, ',');
+      })
+    }
+  }
+
+  countScrollFormat(value) {
+  	let {props: {count}} = this;
+  	return this.formatCount(parseInt(value * (count / 10)));
+  }
+
   render() {
 	return (
-	  <span className="count">{this.props.count}</span>
+	  <Motion defaultStyle={{x: 0}} style={{x: spring(10)}}>
+        {value => {
+          let {x: num} = value;
+          return (<span className="count">{this.countScrollFormat(num)}</span>)
+        }}
+      </Motion>
 	)
   }
 }
